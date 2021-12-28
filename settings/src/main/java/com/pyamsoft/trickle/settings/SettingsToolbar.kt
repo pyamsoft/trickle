9343f853d1c514b37841b3fe24494b8017fc5912
pyamsoft <developer.pyamsoft@gmail.com>
2021-12-27 19:26:12 -0800

    Make settings module

--- settings/src/main/java/com/pyamsoft/trickle/settings/SettingsToolbar.kt
+++ settings/src/main/java/com/pyamsoft/trickle/settings/SettingsToolbar.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-internal fun SettingsToolbar(
+fun SettingsToolbar(
     modifier: Modifier = Modifier,
     onClose: () -> Unit,
 ) {
