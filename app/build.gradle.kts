d861173181e6069c3aaa05cefd58583207b01cd6
pyamsoft <developer.pyamsoft@gmail.com>
2023-12-13 12:12:38 -0800

    Update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
   defaultConfig {
     applicationId = "com.pyamsoft.trickle"
 
-    versionCode = 28
-    versionName = "20231015-1"
+    versionCode = 29
+    versionName = "20231213-1"
 
     minSdk = rootProject.extra["minSdk"] as Int
     targetSdk = rootProject.extra["targetSdk"] as Int
